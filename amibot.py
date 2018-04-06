a2ecee3aceebb39bef4bc707e7f8f45af7286d56
alessio-zamparelli-polito <alessio.zamparelli@studenti.polito.it>
2018-04-06 12:45:13 +0200

    first commit

--- amibot.py
+++ amibot.py
@@ -1,9 +1,24 @@
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction
+from gtts import gTTS
 
 
-def start():
-    #handle start on bot
+def start(bot, update):
+    # handle start on bot
+    update.message.reply_text("Hello!")
 
+
+def echo(bot, update):
+    bot.sendChatAction(update.message.chat_id, ChatAction.UPLOAD_AUDIO)
+    repeat_text = update.message.text
+    update.message.reply_text(repeat_text)
+
+    # convert the textual message into an mp3 file
+    tts = gTTS(text=repeat_text, lang="en")
+    tts.save("echo.mp3")
+
+    # send the message back
+    bot.sendVoice(update.message.chat_id, voice=open("echo.mp3", "rb"))
 
 
 def main():
@@ -12,10 +27,14 @@ def main():
 
     """
     updater = Updater("541907262:AAEOOVHWPnGZPJcHjHEkVtvsJ_asrRdwc14")
-    updater.start_polling()
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
-if __name__ == '__name__':
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
     main()
