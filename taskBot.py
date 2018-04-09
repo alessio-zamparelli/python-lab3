ef09b3ba4e704435e65d5dce2f948257e2554292
alessio-zamparelli <alessio.zamparelli@studenti.polito.it>
2018-04-09 15:35:45 +0200

    first try

--- taskBot.py
+++ taskBot.py
@@ -13,6 +13,7 @@ tasks_list = list()
 
 """
 
+
 def load_list(path):
     with open(path) as f:
         lines = f.readlines()
@@ -26,24 +27,6 @@ def save_list(path):
         f.write("%s\n" % item)
 
 
-def insert_task():
-    print("What?")
-    new_task = input()
-    tasks_list.append(new_task)
-
-
-def remove_task():
-    print("Write the item to delete")
-    to_be_deleted = input()
-    try:
-        tasks_list.remove(to_be_deleted)
-    except ValueError:
-        print("element not found!")
-
-
-
-
-
 def showTasks(bot, update):
     if len(tasks_list) == 0:
         update.message.reply_text("no tasks memorized yet")
@@ -52,10 +35,20 @@ def showTasks(bot, update):
     for element in tasks_list:
         update.message.reply_text(element)
 
+
 def newTask(bot, update):
+    print("What?")
+    new_task = input()
+    tasks_list.append(new_task)
 
 
 def removeTask(bot, update):
+    print("Write the item to delete")
+    to_be_deleted = input()
+    try:
+        tasks_list.remove(to_be_deleted)
+    except ValueError:
+        print("element not found!")
 
 
 def removeAllTasks(bot, update):
@@ -78,14 +71,11 @@ def main():
     dp.add_handler(CommandHandler("removeTask", removeTask))
     dp.add_handler(CommandHandler("removeAllTasks", removeAllTasks))
 
-
     load_list(pathToFile)
     save_list(pathToFile)
 
     updater.start_polling()
     updater.idle()
-
-
 
 
 if __name__ == "__main__":
